1d307dc8dad312dc6fe93554145956c036914bd5
batondesu <101453101+batondesu@users.noreply.github.com>
2023-05-02 02:38:09 +0700

    home update

--- js/main.js
+++ js/main.js
@@ -34,7 +34,127 @@ function addHeader() {
             </div>
         </div>
     </div> 
-    <!-- End Header -->`
-    )
+    <!-- End Header -->
+    `)
 }
 
+function addList() {
+    document.write(`        
+	<div class="main-content">
+        <div class="head-box">
+            <div class="box-header">
+                <div class="gioithieu">
+                    <h1> TRANG THÔNG TIN TÌM VÀ CHO THUÊ PHÒNG TRỌ NHANH CHÓNG HIỆU QUẢ </h1>
+                </div>
+                <div class="listarea"> 
+                    <div class="menu">
+                        <ul class="list-box">
+                            <li><a href="search.html"> <img src="images/box1">Cầu Giấy </a></li>
+                            <li><a href="search.html"> <img src="images/box2">Thanh Xuân </a></li>
+                            <li><a href="search.html"> <img src="images/box3">Đống Đa </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section box box-vip">
+            <div class="container">
+                <div class="hostel hostel-list vip">
+                    <div class="modulecontent">
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item vip"> 
+                            <div class="boder">
+                                <div class="image"></div>
+                                <div class="info box">
+                                    <div class="local"> </div>
+                                    <h4 class="title vip"> </div>
+                                    <div class="location-area"> </div>
+                                    <dl class="contact"> </dl>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    `)
+}
