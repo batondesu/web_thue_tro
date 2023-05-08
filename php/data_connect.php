b41d703f4821adfd8db9b7ed46757083bd917483
phamxuantruong843 <91688786+phamxuantruong843@users.noreply.github.com>
2023-05-08 17:40:54 +0700

    Link database và hướng dẫn cài đặt

---
+++ php/data_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "room_rent";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+$conn -> set_charset('utf8');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
